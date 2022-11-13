c05d3dc6fe8f3ba10d6e3f02203b37bad08295c6
Baptiste Zahnow <75901978+LelouchFR@users.noreply.github.com>
2022-11-13 22:48:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,60 @@
 def encoder(phrase: str) -> str:
     sch: int = 0
     alpha: str = "abcdefghijklmnopqrstuvwxyz"
+    result: str = ""
+    j: int = 0
     for i in range(len(phrase)):
-        index: int = alpha.find(phrase[i])
-        sch = index + i
+        if phrase[i] != chr(32):
+            index: int = alpha.find(phrase[i])
+        sch = index + j
 
-        if i % 2 == 0:
-            sch += 1
-            print(alpha[sch], end='')
+        if j % 2 == 0:
+            if phrase[i] == chr(32):
+                sch = 0
+                j = 0
+                result += " "
+            else:
+                sch += 1
+                result += alpha[sch]
         else:
-            sch -= i * 2
-            print(alpha[sch - 1], end='')
+            if phrase[i] == chr(32):
+                sch = 0
+                j = 0
+                result += " "
+            else:
+                sch -= j * 2
+                result += alpha[sch - 1]
+        j += 1
+    print(result)
 
 def decoder(phrase: str) -> str:
     sch: int = 0
     alpha: str = "abcdefghijklmnopqrstuvwxyz"[::-1]
+    result: str = ""
+    j: int = 0
     for i in range(len(phrase)):
-        index: int = alpha.find(phrase[i])
-        sch = index + i
+        if phrase[i] != chr(32):
+            index: int = alpha.find(phrase[i])
+        sch = index + j
 
-        if i % 2 == 0:
-            sch += 1
-            print(alpha[sch], end='')
+        if j % 2 == 0:
+            if phrase[i] == chr(32):
+                sch = 0
+                j = 0
+                result += " "
+            else:
+                sch += 1
+                result += alpha[sch]
         else:
-            sch -= i * 2
-            print(alpha[sch - 1], end='')
+            if phrase[i] == chr(32):
+                sch = 0
+                j = 0
+                result += " "
+            else:
+                sch -= j * 2
+                result += alpha[sch - 1]
+        j += 1
+    print(result)
 
 def choice(c: int):
     if c == 1:
@@ -32,7 +62,7 @@ def choice(c: int):
     elif c == 2:
         decoder(input("Phrase à decoder: "))
     else:
-        pass
+        print("Quitting...")
 
 if __name__ == "__main__":
     choice(int(input("1 -> Encode\n2 -> Decode\n3 -> Quit\n")))
